b7d4ad4b279ac8ff0f4912b8a4efd4655b3453d4
ashifanassar <ashifanassar@gmail.com>
2024-05-01 04:25:22 +0530

    module

--- docdb.tf
+++ docdb.tf
@@ -1,8 +1,8 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}-docdb"
   engine                  = var.DOCDB_ENGINE_VERSION
-  master_username         = locals.DOCDB_USERNAME
-  master_password         = locals.DOCDB_PASSWORD
+  master_username         = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+  master_password         = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
 #   backup_retention_period = 5
 #   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
