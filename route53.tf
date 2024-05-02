566cb4ac2e462a6875bc937248a71c16d254dfd6
ashifanassar <ashifanassar@gmail.com>
2024-05-02 19:01:30 +0530

    module

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongodb" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
+  zone_id = "Z0547409165EGAKUG3EH3"
+  name    = "mongodb-${var.ENV}.roboshopshopping"
   type    = "CNAME"
   ttl     = 10
   records = [aws_docdb_cluster.docdb.endpoint]
