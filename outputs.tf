6e0f336caf0b57e763716d91411802522a5aa8fc
ashifanassar <ashifanassar@gmail.com>
2024-05-09 22:21:07 +0530

    module

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "DOCDB_ENDPOINT" {
+    value = module.mongodb.DOCDB_ENDPOINT
+}
+# output "REDIS_ENDPOINT" {
+#     value = module.redis.REDIS_ENDPOINT
+# }
+
+# output "MYSQL_ENDPOINT" {
+#     value = module.mysql.MYSQL_ENDPOINT
+# }
