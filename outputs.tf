aeb0958a665b22f693086916a1f47b89a16b49b3
ashifanassar <ashifanassar@gmail.com>
2024-05-10 17:02:16 +0530

    module

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
 output "DOCDB_ENDPOINT" {
     value = aws_docdb_cluster.docdb.endpoint
 }
-# output "REDIS_ENDPOINT" {
-#     value = module.redis.REDIS_ENDPOINT
-# }
-
-# output "MYSQL_ENDPOINT" {
-#     value = module.mysql.MYSQL_ENDPOINT
-# }
